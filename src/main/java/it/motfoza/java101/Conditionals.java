3f1fd160eac3057696e5c154d581806d3492f5d4
Anna Pazera <anna.pazera@tlen.pl>
2016-07-28 14:25:40 +0200

    cwiczenia z if cd

--- src/main/java/it/motfoza/java101/Conditionals.java
+++ src/main/java/it/motfoza/java101/Conditionals.java
@@ -9,21 +9,43 @@ public class Conditionals {
     public static void main(String[] args) {
         boolean b1 = true;
         boolean b2 = false;
-        if (b1) {
-            System.out.println("b1 is true");
-        } else {
-            System.out.println("b1 is false");
+        int wiek=15;
+        boolean jestWGimbazie = wiek>= 13 && wiek <= 16;
+            if (jestWGimbazie){
+                System.out.println("Hurra");
         }
-        if (b2) {
-            System.out.println("b2 is true");
-        } else {
-            System.out.println("b2 is false");
+
+boolean nieJestWGimbazie = wiek <13 || wiek >16;
+        System.out.println(nieJestWGimbazie);
+        nieJestWGimbazie = ! jestWGimbazie;
+
+
+        if (wiek >16 && wiek <20){
+            System.out.println("liceum");
         }
-boolean c = false;
-        if (c=c){
-            System.out.println(" true");
-        }else {
-            System.out.println("false");
-        }
+
+        boolean nieJestWLiceum = wiek <=16 || wiek >=20;
+            System.out.println(nieJestWLiceum);
+        nieJestWLiceum = ! nieJestWLiceum;
+        System.out.println(nieJestWLiceum);
+
+
+
+//        if (b1) {
+//            System.out.println("b1 is true");
+//        } else {
+//            System.out.println("b1 is false");
+//        }
+//        if (b2) {
+//            System.out.println("b2 is true");
+//        } else {
+//            System.out.println("b2 is false");
+//        }
+//boolean c = false;
+//        if (c=c){
+//            System.out.println(" true");
+//        }else {
+//            System.out.println("false");
+//        }
     }
 }
