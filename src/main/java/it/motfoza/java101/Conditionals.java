ee0efd2d372dff1daee6fe16de1cba3137b1a91e
Anna Pazera <anna.pazera@tlen.pl>
2016-07-28 13:52:34 +0200

    cwiczenia z if i boolean

---
+++ src/main/java/it/motfoza/java101/Conditionals.java
@@ -0,0 +1,29 @@
+package it.motfoza.java101;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-28.
+ */
+public class Conditionals {
+
+
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        if (b1) {
+            System.out.println("b1 is true");
+        } else {
+            System.out.println("b1 is false");
+        }
+        if (b2) {
+            System.out.println("b2 is true");
+        } else {
+            System.out.println("b2 is false");
+        }
+boolean c = false;
+        if (c=c){
+            System.out.println(" true");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
