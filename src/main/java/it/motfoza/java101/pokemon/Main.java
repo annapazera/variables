5440b641b75844d6f76a5bc08d7cc4cbf4bc8aeb
Anna Pazera <anna.pazera@tlen.pl>
2016-07-29 16:08:26 +0200

    pokemony zaatakowane

--- src/main/java/it/motfoza/java101/pokemon/Main.java
+++ src/main/java/it/motfoza/java101/pokemon/Main.java
@@ -5,9 +5,36 @@ package it.motfoza.java101.pokemon;
  */
 public class Main {
     public static void main(String[] args) {
-        Pokemon pokemon= new Pokemon();
-        String pokemonDescription=pokemon.toString();
-        System.out.println(pokemonDescription);
+        Pokemon pokemon= new Pokemon("Pikachu");
+        Pokemon pokemon2= new Pokemon("Charizard");
+        Pokemon pokemon3= new Pokemon("Bulbasaur");
+
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
+
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+        pokemon.takeAttack();
+
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
+
+        if (pokemon.isAlive()){
+            System.out.println("Pokemon is alive");
+        } else {
+            System.out.println("Pokemon is unconscious");
+        }
     }
+
 }
 
--- src/main/java/it/motfoza/java101/pokemon/Pokemon.java
+++ src/main/java/it/motfoza/java101/pokemon/Pokemon.java
@@ -5,4 +5,26 @@ package it.motfoza.java101.pokemon;
  */
 public class Pokemon {
 
-}
+    private  String name;
+    private long health =  1000;
+
+    public Pokemon(String name) {
+        this.name = name;
+    }
+    public String toString(){
+        return "Pokemon: " +name + " health: "     + health;
+    }
+
+    public void takeAttack() {
+        health -= 100;
+    }
+
+    public boolean isAlive() {
+       if (health > 0){
+           return true;
+       }
+         else {
+           return false;
+       }
+
+       }}
