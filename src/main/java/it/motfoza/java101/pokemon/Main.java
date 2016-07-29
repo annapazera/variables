ff3569e5e72ea16dfe1e19f3522a9de01b087ee6
Anna Pazera <anna.pazera@tlen.pl>
2016-07-29 15:13:26 +0200

    pokemon first push

---
+++ src/main/java/it/motfoza/java101/pokemon/Main.java
@@ -0,0 +1,13 @@
+package it.motfoza.java101.pokemon;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-29.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pokemon= new Pokemon();
+        String pokemonDescription=pokemon.toString();
+        System.out.println(pokemonDescription);
+    }
+}
+
---
+++ src/main/java/it/motfoza/java101/pokemon/Pokemon.java
@@ -0,0 +1,8 @@
+package it.motfoza.java101.pokemon;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-29.
+ */
+public class Pokemon {
+
+}
